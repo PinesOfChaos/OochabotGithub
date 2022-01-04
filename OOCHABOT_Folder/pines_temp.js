e4444fa36a9fe881674882dbd87c18345fb8cd8e
PinesOfChaos <Pinesolofchaos@gmail.com>
2022-01-04 16:58:51 -0500

    created some items

---
+++ OOCHABOT_Folder/pines_temp.js
@@ -0,0 +1,7 @@
+
+            //ID Name               Type        Description
+create_item('0', 'Potion',          'potion',   'Used to quickly heal 25% of an Oochamon\'s HP')
+create_item('1', 'Hi-Potion',       'potion',   'An advanced potion which heals 50% of an Oochamon\'s HP')
+
+create_item('2', 'Prism',           'prism',    'A device used to capture Oochamon.')
+create_item('3', 'Greater Prism',   'prism',    'An improved prism with a higher capture rate.')
