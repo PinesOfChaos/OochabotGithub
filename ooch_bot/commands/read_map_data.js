3611e2da6160ade94f5c74064903c7deb4c701dc
Jeffdev <robotzurg@gmail.com>
2022-12-20 21:34:40 -0700

    STORY (and beginnings of map data)

--- ooch_bot/commands/read_map_data.js
+++ ooch_bot/commands/read_map_data.js
@@ -8,6 +8,8 @@ module.exports = {
         .setDescription('Read and format map data'),
     async execute(interaction) {
         await interaction.deferReply();
+
+        if (interaction.user.id != '122568101995872256' && interaction.user.id != '145342159724347393') return interaction.reply('This command is not for you.')
         interaction.editReply('Please post the .txt file from the Oochamon Map Editor:tm:')
 
         file_collector = interaction.channel.createMessageCollector({ max: 1, time: 120000 });
@@ -16,6 +18,7 @@ module.exports = {
             if (!file) return console.log('No attached file found');
     
             try {
+                let map_name = file.split('/').slice(-1).replace('.txt', '');
                 // fetch the file from the external URL
                 const response = await fetch(file);
     
@@ -27,61 +30,64 @@ module.exports = {
                 );
     
                 // take the response stream and read it to completion
-                const text = await response.text();
+                let text = await response.text();
     
                 if (text) {
-                    let map_obj, map_name, map_spawn_list, map_trainer_data, map_trainer_loc_list, map_chest_data, map_chest_loc_list, map_data;
-                    
-                    map_obj = text.split('|');
-                    map_name = map_obj[0];
-                    map_spawn_list = map_obj[1].split(',');
-                    map_trainer_data = map_obj[2].split('#');
-                    map_trainer_loc_list = map_obj[3].split(' ');
-                    map_chest_data = map_obj[4].split('#');
-                    map_chest_loc_list = map_obj[5].split(' ');
-                    map_data = map_obj[6].split(' ');
+                    let map_data = text.split('\n');
+                    let data_header = 'err';
+                    let tile_data = [];
+                    let npc_data = [];
+                    let npc_team_data, ooch_data;
+                    let spawn_data = [];
+                    let savepoint_data = [];
+                    let line_data;
+                    for (let line in map_data) {
+                        if (line[0] == '#') {
+                            data_header = line;
+                        } else {
+                            switch (data_header) {
+                                case 'tiles':
+                                    tile_data.push(line.split('|'));
+                                break;
+                                case 'npcs':
+                                    line_data = line.split('|')
+                                    let output = {
+                                        name: line_data[0],
+                                        x: line_data[1],
+                                        y: line_data[2],
+                                        beaten: line_data[3],
+                                        sprite_id: line_data[4],
+                                        coin: line_data[5],
+                                        item_id: line_data[6],
+                                        item_count: line_data[7],
+                                        flag_required: line_data[8],
+                                        flag_given: line_data[9],
+                                        remove_on_finish: line_data[10],
+                                        pre_combat_dialogue: line_data[11],
+                                        player_lost_dialogue: line_data[12],
+                                        player_won_dialogue: line_data[13],
+                                        team: [],
+                                    };
 
-                    map_spawn_list = map_spawn_list.map(v => v = parseInt(v));
+                                    for (let i = 14; i < line_data.length; i++) {
+                                        npc_team_data = line_data[i].split('`');
+                                        ooch_data = db.monster_data.get(parseInt(npc_team_data[0]));
+                                        output.team.push({
+                                            id: parseInt(npc_team_data[0]),
+                                            name: ooch_data.name,
+                                            nickname: npc_team_data[1],
+                                        });
+                                    }
 
-                    map_trainer_data = map_trainer_data.map(v => v.split(','));
-                    for (let v of map_trainer_data) {
-                        v[0] = parseInt(v[0]);
-                        v[4] = v[4].split(';');
-                        v[4] = v[4].map(val => val = parseInt(val));
-                        v[5] = parseInt(v[5]);
-                    };
-
-                    map_trainer_loc_list = map_trainer_loc_list.map(v => v.split(','));
-                    for (let i = 0; i < map_trainer_loc_list.length; i++) {
-                        map_trainer_loc_list[i] = map_trainer_loc_list[i].map(val => val = parseInt(val));
-                    };
-
-                    map_chest_data = map_chest_data.map(v => v.split(','));
-                    for (let v of map_chest_data) {
-                        v[0] = parseInt(v[0]);
-                        v[2] = v[2].split(';');
-                        v[2] = v[2].map(val => val = parseInt(val));
-                    };
-
-                    map_chest_loc_list = map_chest_loc_list.map(v => v.split(','));
-                    for (let i = 0; i < map_chest_loc_list.length; i++) {
-                        map_chest_loc_list[i] = map_chest_loc_list[i].map(val => val = parseInt(val));
-                    };
-
-                    map_data = map_data.map(v => v = v.split(','));
-
-                    map_obj = {
-                        name: map_name,
-                        spawn_list: map_spawn_list,
-                        trainer_list: map_trainer_data,
-                        trainer_loc_list: map_trainer_loc_list,
-                        chest_list: map_chest_data,
-                        chest_loc_list: map_chest_loc_list,
-                        map: map_data
+                                    npc_data.push(output);
+                                break;
+                                case 'spawns':
+                                break;
+                                case 'savepoints':
+                                break;
+                            }
+                        }
                     }
-
-                    console.log(map_obj);
-                    db.maps.set(map_name, map_obj);
 
                     interaction.editReply(`File has been read and data has been parsed. The map has been created.`);
                     msg.delete();
