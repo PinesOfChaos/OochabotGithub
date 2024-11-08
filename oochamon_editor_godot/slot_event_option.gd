5ac7f76c34796354f79495e10d145c10ede6d425
PinesOfChaos <Pinesolofchaos@gmail.com>
2024-11-07 23:25:03 -0500

    Added emote to option button
    see that text ^

--- oochamon_editor_godot/slot_event_option.gd
+++ oochamon_editor_godot/slot_event_option.gd
@@ -4,9 +4,11 @@ extends HBoxContainer
 @onready var line_edit_flag: LineEdit = $LineEditFlag
 @onready var spin_box_price: SpinBox = $SpinBoxPrice
 @onready var option_button: OptionButton = $OptionButton
+@onready var line_edit_emote: LineEdit = $LineEditEmote
 
 var data = {
 	"text" : "",
+	"emote" : "",
 	"event" : "",
 	"flag" : "",
 	"price" : 0,
@@ -16,6 +18,7 @@ var data = {
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	line_edit_text.text = data.text
+	line_edit_emote.text = data.emote
 	line_edit_event_to.text = data.event
 	line_edit_flag.text = data.flag
 	spin_box_price.value = data.price
@@ -41,3 +44,6 @@ func _on_spin_box_price_value_changed(value: float) -> void:
 
 func _on_option_button_item_selected(index: int) -> void:
 	data.style = option_button.get_item_id(index)
+
+func _on_line_edit_emote_text_changed(new_text: String) -> void:
+	data.emote = line_edit_emote.text
