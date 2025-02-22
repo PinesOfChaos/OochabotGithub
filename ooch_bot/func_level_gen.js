1e456374d83e6e9746e0261f1e2a5f3e5259de74
PinesOfChaos <Pinesolofchaos@gmail.com>
2025-02-21 23:40:21 -0500

    added savepoints, shops, chests and npcs to level gen

--- ooch_bot/func_level_gen.js
+++ ooch_bot/func_level_gen.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
 const { OochType } = require("./types");
+const { create_ooch } = require('./func_play.js');
+const db = require("./db.js")
 
 let functions = {
     genmap_theme : function(theme){
@@ -21,11 +23,19 @@ let functions = {
         }
     },
 
-    genmap_new : function(name, width, height, theme, level_min, level_max, spawn_x, spawn_y){
+    genmap_new : function(name, width, height, theme, level_min, level_max, exit_map, exit_x, exit_y, has_savepoints, has_shops){
         let layout = functions.genmap_layout(width, height, Math.ceil(width / 16), Math.ceil(height / 16), 3, 5, theme.map_naturalness);
 
-        
-        let tiles = []
+        let savepoints = [];
+        let spawnzones = [];
+        let transitions = [];
+        let npcs = [];
+        let shops = [];
+        let tiles = [];
+
+        let start_pos = false;
+        let end_pos = false;
+        let misc_positions = [];
         for(let i = 0; i < width; i++){
             tiles[i] = [];
             for(let j = 0; j < height; j++){
@@ -35,17 +45,72 @@ let functions = {
                     case "grass":   tiles[i][j] = theme.tile_grass; break;
                     case "edge":    tiles[i][j] = theme.tile_edge; break;
                     case "decor":   tiles[i][j] = theme.tile_decor; break;
+                    default: 
+                        tiles[i][j] = theme.tile_floor; 
+                        switch(layout[i][j]){
+                            case "start": 
+                                start_pos = {x : i, y : j}; 
+                                break;
+                            case "end": end_pos = {x : i, y : j}; break;
+                            case "misc": misc_positions.push({x : i, y : j}); break;
+                        }
+                    break;
                 }
             }
         }
 
+        //Place a savepoint at the spawn if the map should have one
+        if(has_savepoints){
+            tiles[start_pos.x][start_pos.y] = "t00_001";
+            savepoints.push({
+                is_default : true,
+                x : start_pos.x,
+                y : start_pos.y
+            })
+        }
+
+        //Place a shop next to the spawn if the map should have one
+        if(has_shops){
+            tiles[start_pos.x + 1][start_pos.y] = "t00_007";
+            shops.push({
+                greeting_dialogue : "Main System Initiating Shopkeep Mode",
+                image : "",
+                special_items : [],
+                type : "default",
+                x : start_pos.x + 1,
+                y : start_pos.y
+            })
+        }
+
+        //fill the misc positions with npcs/chests
+        misc_positions = _.shuffle(misc_positions);
+        let npc_count = Math.floor(misc_positions.length * .8)
+        let chest_count = misc_positions.length - npc_count;
+
+        //Add npcs
+        for(let i = 0; i < npc_count; i++){
+            map_npcs.push(functions.genmap_npc(
+                misc_positions[i].x, misc_positions[i].y,
+                level_min, level_max
+            ))
+        }
+
+        //Add chests (picks up where npcs ends)
+        for(let i = npc_count; i < misc_positions.length; i++){
+            map_npcs.push(functions.genmap_chest(
+                misc_positions[i].x, misc_positions[i].y,
+                level_min, level_max
+            ))
+        }
+        
+
         let map_data = {
             map_events : [],
-            map_npcs : [],
-            map_savepoints : [],
-            map_shops : [],
-            map_spawn_zones : [],
-            map_transitions : [],
+            map_npcs : npcs,
+            map_savepoints : savepoints,
+            map_shops : shops,
+            map_spawn_zones : spawnzones,
+            map_transitions : transitions,
 
             map_tiles : tiles,
 
@@ -56,6 +121,166 @@ let functions = {
         }
 
         return(map_data);
+    },
+
+    genmap_empty_npc : function(){
+        return({
+            aggro_range : 0,
+            coin : 0,
+            flag_given : "",
+            flag_required : "",
+            flag_kill : "",
+            remove_on_finish : false,
+            is_catchable : false,
+            items : [],
+            name : "",
+            npc_id : "",
+            
+            pre_combat_dialog: "",
+            post_combat_dialog: "",
+            
+            sprite : 0,
+            sprite_combat : "",
+            sprite_dialog : "",
+            sprite_id : "",
+            team : [],
+            x : x,
+            y : y
+        })
+    },
+
+    genmap_chest : function(x, y, level_min, level_max){
+        let chest_level = _.random(level_min, level_max, false);
+        let chest_num = _.random(1, 3, false);
+        let loot_table = [];
+        let chest_loot = [];
+
+        if(chest_level < 30){
+            loot_table = loot_table.concat([
+                {count : 5, id :  0}, //potion
+                {count : 2, id :  1}, //med-potion
+                {count : 3, id :  3}, //prism
+                {count : 1, id :  4}, //greater prism
+                {count : 2, id :  9}, //eyedrops
+                {count : 2, id : 10}, //shears
+                {count : 2, id : 11}, //daylily
+                {count : 2, id : 12}, //antiparasite
+                {count : 2, id : 13}, //debug chip
+                {count : 2, id : 14}, //cooling balm
+                {count : 2, id : 15}, //nullifying sphere
+                {count : 1, id : 16}, //green boostgem
+                {count : 1, id : 17}, //red boostgem
+                {count : 1, id : 18}, //blue boostgem
+                {count : 1, id : 19}, //yellow boostgem
+                {count : 1, id : 22}, //repulsor
+                {count : 1, id : 23}, //emergency teleporter  
+            ])
+        }
+        if(chest_level > 20){
+            loot_table = loot_table.concat([
+                {count : 5, id :  1}, //med-potion
+                {count : 2, id :  2}, //hi-potion
+                {count : 3, id :  4}, //greater prism
+                {count : 1, id :  5}, //grand prism
+                {count : 2, id :  9}, //eyedrops
+                {count : 2, id : 10}, //shears
+                {count : 2, id : 11}, //daylily
+                {count : 2, id : 12}, //antiparasite
+                {count : 2, id : 13}, //debug chip
+                {count : 2, id : 14}, //cooling balm
+                {count : 2, id : 15}, //nullifying sphere
+                {count : 2, id : 23}, //emergency teleporter  
+            ])
+        }
+        if(chest_level >= 30){
+            loot_table = loot_table.concat([
+                {count : 5, id :  1}, //med-potion
+                {count : 5, id :  2}, //hi-potion
+                {count : 5, id :  4}, //greater prism
+                {count : 3, id :  5}, //grand prism
+                {count : 1, id :  7}, //attack crystal
+            ])
+        }
+        if(chest_level >= 40){
+            loot_table = loot_table.concat([
+                {count : 5, id :  2}, //hi-potion
+                {count : 5, id :  5}, //grand prism
+                {count : 1, id :  6}, //perfect prism
+                {count : 1, id :  7}, //attack crystal
+            ])
+        }
+
+        for(let i = 0; i < chest_num; i++){
+            chest_loot.push(_.sample(loot_table));
+        }
+
+
+
+        let npc = functions.genmap_empty_npc();
+        npc.x = x;
+        npc.y = y;
+        npc.aggro_range = 0;
+        npc.items = chest_loot;
+        npc.sprite_id = "c_013";
+        npc.npc_id = "1234567890";
+        npc.name = "Chest";
+        npc.pre_combat_dialog = "You opened the chest...";
+        npc.post_combat_dialog = "";
+        npc.remove_on_finish = true;
+
+        return npc;
+    },
+
+    genmap_npc : function(x, y, level_min, level_max){
+        
+        const lerp = (x, y, a) => x * (1 - a) + y * a;
+
+        let teamsize = Math.floor(Math.random() * 3) + 1;
+        let avg_level = lerp(level_max, level_min, teamsize/4); //more team, lower avg level
+
+        //TODO get this as an array
+        let npc_ooch_options = db.monster_data.values();
+        npc_ooch_options.filter((mon) => 
+            mon.evo_stage > 0 && //remove all mons that are evolved (we will evolve them later)
+            ![ //remove legendaries and special mons the player shouldnt see
+                34, //Purif-i
+                -1, //i
+                105, //Nullifly,
+                96, //Tryptid
+                97, //Roswier
+                98, //Chemerai
+            ].includes(mon.id));
+
+        //Add mons to the npc's team
+        let team = []
+        for(let i = 0; i < teamsize; i++){
+            let ooch = _.sample(npc_ooch_options);
+            let ooch_level = avg_level + _.random(-2, 2, false);
+
+            //Evolve the mon if possible
+            let evo_steps = 0;
+            while(ooch.evo_level < ooch_level && ooch.evo_level != -1 && evo_steps < 5){
+                if(random() < .5){
+                    ooch = db.monster_data.get(ooch.evo_id);
+                }
+                evo_steps++;
+            }
+            team.push(create_ooch(ooch.id, ooch_level));
+        }
+
+        let npc = functions.genmap_empty_npc()
+        npc.x = x;
+        npc.y = y;
+        npc.aggro_range = 3;
+        npc.team = team;
+        npc.sprite_id = "c00_005";
+        npc.npc_id = "1234567890";
+        npc.name = "Wandering Trainer";
+        pre_combat_dialog = "We must fight!"
+        post_combat_dialog = "*The trainer suddenly vanished...*"
+        npc.remove_on_finish = true;
+
+        return npc;
     },
 
     genmap_layout : function(width, height, room_cols, room_rows, room_size_avg, naturalness){
@@ -71,6 +296,20 @@ let functions = {
             }
         }
 
+        
+        //Randomize x_start/end
+        let xrand =  Math.random() > .5;
+        let start_x = xrand ? 0 : room_cols - 1;
+        let end_x = xrand ? room_cols - 1 : 0;
+        
+        let yrand =  Math.random() > .5;
+        let start_y = yrand ? 0 : room_rows - 1;
+        let end_y = yrand ? room_rows - 1 : 0;
+        
+        let start_pos = {x : 0, y : 0};
+        let end_pos = {x : 0, y : 0};
+        let optional_spots = [];
+
         //Decide on rooms
         let rooms = [];
         let room_space_x = width / (room_cols + 1);
@@ -84,16 +323,29 @@ let functions = {
                 //point left if not leftmost, point up if not upmost, otherwise point nowhere
                 //this should connect all nodes by default
 
+                let room_center_x = Math.floor((i * room_space_x) + room_space_x + (Math.random() * 8 * (naturalness + .5)) - 1);
+                let room_center_y = Math.floor((j * room_space_y) + room_space_y + (Math.random() * 8 * (naturalness + .5)) - 1)
+
                 rooms.push({
                     roomgridx : i,
                     roomgridy : j,
-                    x : Math.floor((i * room_space_x) + room_space_x + (Math.random() * 8 * (naturalness + .5)) - 1),
-                    y : Math.floor((j * room_space_y) + room_space_y + (Math.random() * 8 * (naturalness + .5)) - 1),
+                    x : room_center_x,
+                    y : room_center_y,
                     width : Math.round((room_size_avg/2) + (Math.random() * 2) - 1),
                     height : Math.round((room_size_avg/2) + Math.random() - .5),
                     coord_to : coord_to
                     
                 })
+
+                if(i == start_x && j == start_y){
+                    start_pos = {x : room_center_x, y : room_center_y}
+                }
+                else if(i == end_x && j == end_y){
+                    end_pos = {x : room_center_x, y : room_center_y}
+                }
+                else {
+                    optional_spots.push({x : room_center_x, y : room_center_y});
+                }
             }
         }
 
@@ -262,6 +514,15 @@ let functions = {
         }
 
         
+        //Add start and end points
+        layout[start_pos.x][start_pos.y] = "start";
+        layout[end_pos.x][end_pos.y] = "end";
+
+        //Add optional content spots
+        for(let spot of optional_spots){
+            layout[spot.x][spot.y] = "misc";
+        }
+
         if(true){ //This section is for testing purposes, make false if not testing
             var layout_text = '';
             for(let j = 1; j < height; j++){
@@ -272,6 +533,10 @@ let functions = {
                         case "grass":   layout_text += "ww"; break;
                         case "edge":    layout_text += "II"; break;
                         case "decor":   layout_text += "oo"; break;
+                        
+                        case "misc":    layout_text += "XX"; break;
+                        case "start":   layout_text += "SS"; break;
+                        case "end":     layout_text += "EE"; break;
                     }
                 }
                 layout_text += "\n"
@@ -281,7 +546,7 @@ let functions = {
         }
         return layout;
     }
-    
+
 }
 
 module.exports = functions;
